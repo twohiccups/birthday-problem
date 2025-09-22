8e253ef482a2a1727eeff9a02c48c4bd7c2f3772
Mark <koshkinmark@gmail.com>
2025-09-22 19:03:06 -0400

    feat: unused component

--- public/assets/components/FunIntro.tsx
+++ public/assets/components/FunIntro.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import {useRef, useState } from "react";
 import { motion } from "motion/react";
 import Image from "next/image";
 import Link from "next/link";
